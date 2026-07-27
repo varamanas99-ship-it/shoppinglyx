f3d8e138d8b8539085fc17b6df2ce37c78f542f2
Vara Manas <varamanas99@gmail.com>
2026-07-27 10:51:36 +0530

    base page updated

--- app/admin.py
+++ app/admin.py
@@ -10,25 +10,32 @@ from .models import (
 @admin.register(Customer)
 class CustomerModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'name', 'locality', 'city', 'zipcode', 'state']
+    readonly_fields = ['user']
+
 
 # ==================== 🆕 MULTIPLE IMAGES INLINE ==================== #
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 3  
 
+
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'selling_price', 'discounted_price', 'description', 'brand', 'category', 'product_image']
     inlines = [ProductImageInline]  
 
+
 @admin.register(Cart) 
 class CartModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'product', 'quantity'] 
+    readonly_fields = ['user', 'product', 'quantity']
+    raw_id_fields = ['user', 'product']
+
 
 @admin.register(OrderPlaced)
 class OrderPlacedModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'customer', 'product', 'quantity', 'ordered_date', 'status']  
     
-    # 🆕 આનાથી ડ્રોપડાઉન અને પેન્સિલ/પ્લસ/આંખના બધા જ બટનો ગાયબ થઈ જશે અને માત્ર રીડ-ઓનલી ટેક્સ્ટ કે આઈડી જ દેખાશે
-    readonly_fields = ['user', 'customer', 'product', 'quantity']
+    # Aa read-only fields thi dropdown ane badha buttons gayab thai jashe
+    readonly_fields = ['user', 'customer', 'product', 'quantity', 'ordered_date']
     raw_id_fields = ['user', 'customer', 'product']
